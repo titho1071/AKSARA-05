a232d9a528c213a97981bc4546b3b81cdb5d14fe
titho1071 <tithoilham@gmail.com>
2026-07-21 11:34:04 +0700

    Trust Railway proxy for HTTPS detection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
+    
     })->create();
